7b657fd3533f40822908ef60d6a56c4740f7057b
sabol72 <sabal72sc@gmail.com>
2025-03-03 18:30:06 +0545

    day 15 update

--- Medieval/assets/js/script.js
+++ Medieval/assets/js/script.js
@@ -1,39 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const swiper = new Swiper(".swiper", {
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    autoplay: {
-      delay: 3000,
-    },
-    navigation: {
-      nextEl: ".custom-button-next",
-      prevEl: ".custom-button-prev",
-    },
-    slidesPerView: 4,
-    spaceBetween: 10,
-    breakpoints: {
-      1024: {
-        slidesPerView: 4,
-      },
-      768: {
-        slidesPerView: 3,
-      },
-      480: {
-        slidesPerView: 2,
-      },
-      0: {
-        slidesPerView: 1,
-      },
-    },
-  });
-
   const menu = document.querySelector(".nav-menu");
+  const navBar = document.querySelector("nav");
   const hamburger = document.querySelector(".hamburger-menu");
   const menuLinks = document.querySelectorAll(".nav-links-item");
-
   // Toggle Menu
   function toggleMenu() {
     menu.classList.toggle("show");
@@ -47,8 +16,17 @@ document.addEventListener("DOMContentLoaded", function () {
     document.body.classList.remove("no-scroll");
     hamburger.classList.remove("active");
   }
-
+  function transparentNav() {
+    navBar.classList.add("scrolled");
+  }
   // Event Listeners
   hamburger.addEventListener("click", toggleMenu);
   menuLinks.forEach((link) => link.addEventListener("click", closeMenu));
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 50) {
+      navBar.classList.add("scrolled");
+    } else {
+      navBar.classList.remove("scrolled");
+    }
+  });
 });
